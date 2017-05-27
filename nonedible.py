579911bd0dc0fdd74b92c993e68e751df1ab1e07
VAT2\VAT <vietzerg@gmail.com>
2017-05-28 00:38:58 +0700

    Updated animations for NonEdible.

--- nonedible.py
+++ nonedible.py
@@ -11,7 +11,6 @@ class NonEdible:
     def __init__(self):
         self.active = True
         self.position = Point()
-        #self.image_path = [ "resources/non_edible/non_edible_{0}.png".format(i) for i in range(1, 4) ]
         self.renderer = InfiniAnimation("resources/non_edible/")
         self.renderer.staterender.state = "move"
         self.box_collider = BoxCollider(self.position, self.renderer.width, self.renderer.height)
