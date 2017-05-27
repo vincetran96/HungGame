6eb5c03cbc129913ece487db29bb2f5896d19fb1
VAT2\VAT <vietzerg@gmail.com>
2017-05-27 13:18:02 +0700

    Edit edibles

--- edible.py
+++ edible.py
@@ -10,3 +10,11 @@ class Edible:
 		self.active = True
 		self.position = Point()
 		self.renderer = loadSpriteRenderer("resources...")
+		self.box_collider = BoxCollider(self.position, self.renderer.width, self.renderer.height)
+		self.constraints = None
+
+	def run(self):
+		self.position.add_up(0, 10)
+		target = physics.check_contact(self.box_collider)
+		if target is not None and type(target) is Player:
+			self.active = False
